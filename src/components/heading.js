b51ada089c1bfc88aedca23bede68b15f9e6fe21
chrysilla mayasari <chrysilla_mysr@hotmail.com>
2021-02-23 00:22:52 +0700

    add useless counter

--- src/components/heading.js
+++ src/components/heading.js
@@ -1,5 +1,6 @@
 import React from "react"
+import tw from "twin.macro"
 
 export default function Heading(props) {
-  return <h1>{props.headerText}</h1>
+  return <h1 css={tw`text-5xl`}>{props.headerText}</h1>
 }
--- src/pages/contact.js
+++ src/pages/contact.js
@@ -6,16 +6,24 @@ import tw, { styled } from "twin.macro"
 
 export default function Contact() {
   let [counter, setCounter] = useState(0)
+  let [step, setStep] = useState(1)
 
-  const handleAddCount = (e) => {
+  const handleAddCount = e => {
     e.preventDefault()
-    setCounter(counter + 1)
+    setCounter(counter + step)
   }
 
-  const handleReduceCount = (e) => {
+  const handleReduceCount = e => {
     e.preventDefault()
-    setCounter(counter - 1)
+    setCounter(counter - step)
   }
+
+  const handleChange = e => {
+    e.preventDefault()
+    setStep(parseInt(e.target.value) || '')
+  }
+
+  const coloredCounter = counter % 10 == 0 ? tw`text-red-500` : tw`text-blue-500`
 
   return (
     <Layout>
@@ -25,15 +33,20 @@ export default function Contact() {
         <p>
           <a href="mailto:me@example.com">me@example.com</a>
         </p>
-        <button css={tw`shadow-lg p-2 rounded bg-green-300 mt-4`}>This is button</button>
-        <div css={tw`mt-4`}>
+        <button css={tw`shadow-lg p-2 rounded bg-green-300 mt-4`}>This is a useless button</button>
+        <h2 css={tw`mt-4 text-4xl`}>Useless Counter</h2>
+        <div>
+          <p>Total: <span css={[tw`text-2xl font-bold`, coloredCounter]}>{counter}</span></p><br/>
           <button
-            css={tw`shadow-lg p-2 rounded bg-purple-300 mr-2`}
-            onClick={handleReduceCount}>Reduce</button>
+            css={tw`shadow-lg py-2 px-4 rounded bg-purple-300 mr-2`}
+            onClick={handleReduceCount}>-</button>
           <button
-            css={tw`shadow-lg p-2 rounded bg-purple-300 mr-2`}
-            onClick={handleAddCount}>Add</button>
-          <span>{counter}</span>
+            css={tw`shadow-lg py-2 px-4 rounded bg-purple-300 ml-2`}
+            onClick={handleAddCount}>+</button>
+          <br/><br/>
+          <label>Step (Number only)</label><br/>
+          <input type="number" name="step" id="step" value={step} pattern="^-?[0-9]\d*\.?\d*$"
+            onChange={handleChange} />
         </div>
       </div>
     </Layout>
